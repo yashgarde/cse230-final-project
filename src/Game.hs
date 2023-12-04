8222d7277bd1d534fd541aa5a2b2e6522166db17
Yash Garde <36286221+yashgarde@users.noreply.github.com>
2023-12-04 02:37:55 +0000

    Game board first pass

--- src/Game.hs
+++ src/Game.hs
@@ -5,10 +5,9 @@ module Game where
 
 import Graphics.Vty
 import Brick
-import Brick.Widgets.Core
-import Brick.Main
-import Brick.Types
-import Brick.AttrMap
+import Brick.Widgets.Border
+import Brick.Widgets.Center
+import Brick.Widgets.Border.Style (unicodeRounded)
 
 game :: IO ()
 game = do
@@ -18,7 +17,7 @@ game = do
 
 
 data GameState =
-    GameState { 
+    GameState {
         board :: [[Int]],
         score :: Int
     }
@@ -41,12 +40,23 @@ gameApp =
     }
 
 buildInitState :: IO GameState
-buildInitState = return GameState {board = [[]]}
+-- Currently just returns a hardcoded board -- TODO: Make this random
+buildInitState = return GameState {board = [[0,0,0,0],[0,2,0,0],[0,0,2,0],[0,0,0,0]], score = 0}
 
 drawGame :: GameState -> [Widget ResName]
-drawGame state = []
+drawGame state = [
+    withBorderStyle unicodeRounded $ borderWithLabel (str "2048") $ (vBox $ map (hCenter . drawBoardRow) (board state)) <=> (padTop (Pad 1) $ hCenter $ str $ "Current Score: " ++ show (score state))
+    ]
 
+drawBoardRow :: [Int] -> Widget ResName
+drawBoardRow row = hBox $ map drawCell row
 
+drawCell :: Int -> Widget ResName
+drawCell val = 
+    let cellDisp = if val == 0 then " " else show val in
+    hLimit 10 $ withBorderStyle unicodeRounded $ border $ hCenter $ padAll 1 $ str cellDisp
+
+handleGameEvent :: BrickEvent n1 e -> EventM n2 s ()
 handleGameEvent e =
     case e of
         VtyEvent vte ->
